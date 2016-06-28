d208392700944b57099b8bf213edc54216f26247
pardox7takano <pardox7takano@gmail.com>
2016-06-29 00:57:09 +0900

    日付型の拡張メソッド用クラスの追加
    日付型の拡張メソッド用クラスの追加します。

---
+++ Cactus.Core/Cactus.Core.UnitTest/Extensions/DateTimeExtensionUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cactus.Core.UnitTest
+{
+    [TestClass]
+    public class DateTimeExtensionUnitTest
+    {
+        [TestMethod]
+        public void ToBusinessStartDateTest()
+        {
+            var actual = new DateTime(2016, 8, 10);
+            Assert.AreEqual(new DateTime(2016, 4, 1), actual);
+        }
+    }
+}
---
+++ Cactus.Core/Cactus.Core/Extensions/DateTimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cactus.Core
+{
+    public static class DateTimeExtension
+    {
+
+        public static DateTime ToBusinessStartDate(this DateTime value)
+        {
+            return value;
+        }
+    }
+}
